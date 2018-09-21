c6aaf65e0de12bd477f7e2ba811ff70cd8a27713
Strandberg Filip <filip.strandberg@tieto.com>
2018-09-21 07:57:47 +0200

    Show popup when pressing ctrl+shift+f

--- Extension/background.js
+++ Extension/background.js
@@ -5,14 +5,15 @@ chrome.runtime.onInstalled.addListener(function() {
 		console.log("The color is green.");
 	});
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-    	conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+  	conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'developer.chrome.com'},
+      })
+      ],
+          actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+  
 });
+
